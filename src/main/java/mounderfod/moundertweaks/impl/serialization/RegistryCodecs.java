ea3c92f0e3012238ae1a69a1a99165ef86297c34
SD <harrypotter.starwars.shrish@gmail.com>
2020-09-21 20:12:16 +0530

    comapFlatMap -> xmap
    the registry is defaulted
    
     Changes to be committed:
    	modified:   src/main/java/mounderfod/moundertweaks/impl/serialization/RegistryCodecs.java

--- src/main/java/mounderfod/moundertweaks/impl/serialization/RegistryCodecs.java
+++ src/main/java/mounderfod/moundertweaks/impl/serialization/RegistryCodecs.java
@@ -1,8 +1,6 @@
 package mounderfod.moundertweaks.impl.serialization;
 
 import com.mojang.serialization.Codec;
-import com.mojang.serialization.DataResult;
-import com.mojang.serialization.Lifecycle;
 
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
@@ -10,6 +8,6 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public interface RegistryCodecs {
-    Codec<Block> BLOCK = Identifier.CODEC.comapFlatMap((id) -> DataResult.success(Registry.BLOCK.get(id), Lifecycle.stable()), Registry.BLOCK::getId);
-    Codec<Item> ITEM = Identifier.CODEC.comapFlatMap((id) -> DataResult.success(Registry.ITEM.get(id), Lifecycle.stable()), Registry.ITEM::getId);
+    Codec<Block> BLOCK = Identifier.CODEC.xmap(Registry.BLOCK::get, Registry.BLOCK::getId);
+    Codec<Item> ITEM = Identifier.CODEC.xmap(Registry.ITEM::get, Registry.ITEM::getId);
 }
